64b27d60f3e514fb702b81cabff6e401f0d3e820
vishu handa <vishuhanda001@gmail.com>
2017-12-12 18:13:04 +0530

    done final quantity bugs also on app and added funcitonality if local storage

--- frontnotes/src/app/addtocart/addtocart.component.ts
+++ frontnotes/src/app/addtocart/addtocart.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ProductService} from '../shared/product.service';
+import { ProductService } from '../shared/product.service';
 import { Product } from '../product';
 
 
@@ -12,170 +12,190 @@ export class AddtocartComponent implements OnInit {
 
 
 
-productsinCart:Product[]=[];
-totalproductsincart:number=0;
-subTotal = 0;
-tax = 0;
-total=0;
-defaultval = 1;
-  constructor(public productService:ProductService) {
-      this.productService.cartItems.subscribe((products:Product[])=>{
-                   if(typeof products == "function" ){
-                      
-                      this.productsinCart=JSON.parse(this.getDataFromLocalStorage("products"));
-                      this.totalproductsincart = this.productsinCart.length;
-                    }
-                    if(typeof products == "object" ){
-                      
-                      this.productsinCart = products;
-                      this.totalproductsincart = this.productsinCart.length;
+  productsinCart: Product[] = [];
+  totalproductsincart: number = 0;
+  subTotal = 0;
+  tax = 0;
+  total = 0;
+  
+  constructor(public productService: ProductService) {
+    this.productService.cartItems.subscribe((products: Product[]) => {
+      if (typeof products == "function") {
 
-                    this.setDatatoLocalStorage("products",JSON.stringify(products));
+        this.productsinCart = JSON.parse(this.getDataFromLocalStorage("products"));
+        this.totalproductsincart = this.productsinCart.length;
 
-                    for(var i=0;i<this.productsinCart.length;i++){
-                      this.subTotal = this.subTotal + this.productsinCart[i].productAmount;
-                    }
+        this.subTotal = JSON.parse(this.getDataFromLocalStorage("subTotal"));
+        this.tax = JSON.parse(this.getDataFromLocalStorage("tax"));
+        this.total = JSON.parse(this.getDataFromLocalStorage("total"));
 
-                      this.tax = this.subTotal*19/100;
-                      this.total = this.subTotal+this.tax;
+      }
+      if (typeof products == "object") {
 
-                      this.setDatatoLocalStorage("subTotal",JSON.stringify(this.subTotal))
-                      this.setDatatoLocalStorage("tax",JSON.stringify(this.tax))
-                      this.setDatatoLocalStorage("total",JSON.stringify(this.total))
+        this.productsinCart = products;
+        this.totalproductsincart = this.productsinCart.length;
+
+        this.setDatatoLocalStorage("products", JSON.stringify(products));
+
+        for (var i = 0; i < this.productsinCart.length; i++) {
+          this.subTotal = this.subTotal + this.productsinCart[i].productAmount;
+        }
+
+        this.tax = this.subTotal * 19 / 100;
+        this.total = this.subTotal + this.tax;
+
+        this.setDatatoLocalStorage("subTotal", JSON.stringify(this.subTotal))
+        this.setDatatoLocalStorage("tax", JSON.stringify(this.tax))
+        this.setDatatoLocalStorage("total", JSON.stringify(this.total))
 
 
 
-                 }
-   
-      })
-   }
+      }
+
+    })
+  }
 
   ngOnInit() {
-    
+
   }
 
-  deleteProduct(product:Product){
-    let index ;  
-      for(var i=0;i<this.productsinCart.length;i++){
-        if(this.productsinCart[i].productId==product.productId){
-          index = i;
+  deleteProduct(product: Product) {
+    let index;
+    for (var i = 0; i < this.productsinCart.length; i++) {
+      if (this.productsinCart[i].productId == product.productId) {
+        index = i;
+      }
+    }
+    this.productsinCart.splice(index, 1);
+    this.totalproductsincart = this.productsinCart.length;
+    this.setDatatoLocalStorage("products", JSON.stringify(this.productsinCart));
+
+    this.subTotal = 0;
+    for (var i = 0; i < this.productsinCart.length; i++) {
+      this.subTotal = this.subTotal + this.productsinCart[i].productAmount;
+    }
+
+    this.tax = this.subTotal * 19 / 100;
+    this.total = this.subTotal + this.tax;
+
+    this.setDatatoLocalStorage("subTotal", JSON.stringify(this.subTotal))
+    this.setDatatoLocalStorage("tax", JSON.stringify(this.tax))
+    this.setDatatoLocalStorage("total", JSON.stringify(this.total))
+
+
+  }
+
+
+  decrementQuantity(e, product) {
+    if (parseInt(e.innerText) < 2) {
+      alert("cant do that");
+      return;
+    }
+    e.innerText = parseInt(e.innerText) - 1;
+    
+    this.productsinCart = JSON.parse(localStorage.getItem("products"));
+
+    for(var i=0;i<this.productsinCart.length;i++){
+      if(this.productsinCart[i].productId == product.productId){
+        this.productsinCart[i].productQuantity = this.productsinCart[i].productQuantity-1;
+      }
+  }
+
+    for (var i = 0; i < this.productsinCart.length; i++) {
+      if (this.productsinCart[i].productId == product.productId) {
+        this.productsinCart[i].productAmount = this.productsinCart[i].productAmount * parseInt(e.innerText);
+      }
+    }
+
+    this.setDatatoLocalStorage("products",JSON.stringify(this.productsinCart));
+
+    this.subTotal = 0;
+    for (var i = 0; i < this.productsinCart.length; i++) {
+      this.subTotal = this.subTotal + this.productsinCart[i].productAmount;
+    }
+
+    this.tax = this.subTotal * 19 / 100;
+    this.total = this.subTotal + this.tax;
+
+    this.setDatatoLocalStorage("subTotal", JSON.stringify(this.subTotal))
+    this.setDatatoLocalStorage("tax", JSON.stringify(this.tax))
+    this.setDatatoLocalStorage("total", JSON.stringify(this.total))
+
+
+
+
+  }
+
+  incrementQuantity(e, product) {
+    if (parseInt(e.innerText) > 25) {
+      alert("cant do that");
+      return;
+    }
+    e.innerText = parseInt(e.innerText) + 1;
+    this.productsinCart = JSON.parse(localStorage.getItem("products"));
+    
+    for(var i=0;i<this.productsinCart.length;i++){
+        if(this.productsinCart[i].productId == product.productId){
+          this.productsinCart[i].productQuantity = this.productsinCart[i].productQuantity+1;
         }
-      }
-      this.productsinCart.splice(index,1);
-      this.totalproductsincart = this.productsinCart.length;
-      this.setDatatoLocalStorage("products",JSON.stringify(this.productsinCart));
-
-      this.subTotal = 0;
-      for(var i=0;i<this.productsinCart.length;i++){
-        this.subTotal = this.subTotal + this.productsinCart[i].productAmount;
-      }
-
-        this.tax = this.subTotal*19/100;
-        this.total = this.subTotal+this.tax;
-
-        this.setDatatoLocalStorage("subTotal",JSON.stringify(this.subTotal))
-        this.setDatatoLocalStorage("tax",JSON.stringify(this.tax))
-        this.setDatatoLocalStorage("total",JSON.stringify(this.total))
-
-
-  }
-
-
-  decrementQuantity(e,product){
-    if(parseInt(e.innerText) <2){
-      alert("cant do that");
-      return; 
     }
-    e.innerText = parseInt(e.innerText) -1;
-    this.defaultval = this.defaultval-1;
-    
-    this.productsinCart = JSON.parse(localStorage.getItem("products"));       
-    for(var i=0;i<this.productsinCart.length;i++){
-      if(this.productsinCart[i].productId==product.productId){
-        this.productsinCart[i].productAmount = this.productsinCart[i].productAmount*parseInt(e.innerText);
+
+    for (var i = 0; i < this.productsinCart.length; i++) {
+      if (this.productsinCart[i].productId == product.productId) {
+        this.productsinCart[i].productAmount = this.productsinCart[i].productAmount * parseInt(e.innerText);
       }
     }
 
-    this.subTotal = 0;
-    for(var i=0;i<this.productsinCart.length;i++){
-      this.subTotal = this.subTotal + this.productsinCart[i].productAmount;
-    }
-
-      this.tax = this.subTotal*19/100;
-      this.total = this.subTotal+this.tax;
-
-      this.setDatatoLocalStorage("subTotal",JSON.stringify(this.subTotal))
-      this.setDatatoLocalStorage("tax",JSON.stringify(this.tax))
-      this.setDatatoLocalStorage("total",JSON.stringify(this.total))
-
-
-
-
-  }
-  
-  incrementQuantity(e,product){
-    if(parseInt(e.innerText) >25){
-      alert("cant do that");
-      return; 
-    }
-    e.innerText = parseInt(e.innerText) +1;   
-    this.defaultval = this.defaultval+1; 
-
-    this.productsinCart = JSON.parse(localStorage.getItem("products"));       
-    for(var i=0;i<this.productsinCart.length;i++){
-      if(this.productsinCart[i].productId==product.productId){
-        this.productsinCart[i].productAmount = this.productsinCart[i].productAmount*parseInt(e.innerText);
-      }
-    }
+    this.setDatatoLocalStorage("products",JSON.stringify(this.productsinCart));
 
 
     this.subTotal = 0;
-    for(var i=0;i<this.productsinCart.length;i++){
+    for (var i = 0; i < this.productsinCart.length; i++) {
       this.subTotal = this.subTotal + this.productsinCart[i].productAmount;
     }
 
-      this.tax = this.subTotal*19/100;
-      this.total = this.subTotal+this.tax;
+    this.tax = this.subTotal * 19 / 100;
+    this.total = this.subTotal + this.tax;
 
-      this.setDatatoLocalStorage("subTotal",JSON.stringify(this.subTotal))
-      this.setDatatoLocalStorage("tax",JSON.stringify(this.tax))
-      this.setDatatoLocalStorage("total",JSON.stringify(this.total))
+    this.setDatatoLocalStorage("subTotal", JSON.stringify(this.subTotal))
+    this.setDatatoLocalStorage("tax", JSON.stringify(this.tax))
+    this.setDatatoLocalStorage("total", JSON.stringify(this.total))
 
 
 
   }
 
-  getvalue(e){
+  getvalue(e) {
     console.log(e);
   }
 
-  selectQuantity(e:any,product:Product){
-     this.productsinCart = JSON.parse(localStorage.getItem("products"));       
-    for(var i=0;i<this.productsinCart.length;i++){
-      if(this.productsinCart[i].productId==product.productId){
-        this.productsinCart[i].productAmount = this.productsinCart[i].productAmount*e.value;
+  selectQuantity(e: any, product: Product) {
+    this.productsinCart = JSON.parse(localStorage.getItem("products"));
+    for (var i = 0; i < this.productsinCart.length; i++) {
+      if (this.productsinCart[i].productId == product.productId) {
+        this.productsinCart[i].productAmount = this.productsinCart[i].productAmount * e.value;
       }
     }
 
     this.subTotal = 0;
-    for(var i=0;i<this.productsinCart.length;i++){
+    for (var i = 0; i < this.productsinCart.length; i++) {
       this.subTotal = this.subTotal + this.productsinCart[i].productAmount;
     }
 
-      this.tax = this.subTotal*19/100;
-      this.total = this.subTotal+this.tax;
+    this.tax = this.subTotal * 19 / 100;
+    this.total = this.subTotal + this.tax;
 
-      this.setDatatoLocalStorage("subTotal",JSON.stringify(this.subTotal))
-      this.setDatatoLocalStorage("tax",JSON.stringify(this.tax))
-      this.setDatatoLocalStorage("total",JSON.stringify(this.total))
+    this.setDatatoLocalStorage("subTotal", JSON.stringify(this.subTotal))
+    this.setDatatoLocalStorage("tax", JSON.stringify(this.tax))
+    this.setDatatoLocalStorage("total", JSON.stringify(this.total))
 
   }
 
-  getDataFromLocalStorage(key:string){
+  getDataFromLocalStorage(key: string) {
     return this.productService.getlocalstoragedata(key);
   }
-  setDatatoLocalStorage(key:string,value:string){
-    this.productService.setlocalstoragedata(key,value);
+  setDatatoLocalStorage(key: string, value: string) {
+    this.productService.setlocalstoragedata(key, value);
   }
 
 }
--- frontnotes/src/app/product.ts
+++ frontnotes/src/app/product.ts
@@ -3,14 +3,16 @@ export class Product{
         public productId:number,
         public productName:string,
         public productImage:string,
-        public productAmount:number
+        public productAmount:number,
+        public productQuantity:number
     ){}
 
     getproduct(){
         return {"productId":this.productId,
                 "productName":this.productName,
                 "productImage":this.productImage,
-                "productAmount":this.productAmount}
+                "productAmount":this.productAmount,
+                "productQuantity":this.productQuantity}
     }
 
 }
